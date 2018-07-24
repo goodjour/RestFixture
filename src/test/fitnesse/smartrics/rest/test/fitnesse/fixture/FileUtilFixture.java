df9797251ade23227bde2271c962471ae1f63231
goodjour <goodjour@gmail.com>
2018-07-24 08:58:30 -0400

    Update FileUtilFixture.java

--- src/test/fitnesse/smartrics/rest/test/fitnesse/fixture/FileUtilFixture.java
+++ src/test/fitnesse/smartrics/rest/test/fitnesse/fixture/FileUtilFixture.java
@@ -2,7 +2,7 @@
  *
  *  This file is part of RestFixture.
  *
- *  RestFixture (http://code.google.com/p/rest-fixture/) is free software:
+ *  RestFixture (http://code.google.com/p/rest-fixture/) is fr ee software:
  *  you can redistribute it and/or modify it under the terms of the
  *  GNU Lesser General Public License as published by the Free Software Foundation,
  *  either version 3 of the License, or (at your option) any later version.
